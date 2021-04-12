e2eaff4df62e3dd6e1428d5b7a9d0fcb53add246
WX <42308734@qq.com>
2021-04-12 10:20:27 +0800

    fix:集成dva

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -7,10 +7,18 @@ import "taro-ui/dist/style/components/button.scss" // 按需引入
 // eslint-disable-next-line
 const Index = React.forwardRef((props, ref) => {
   const { global: { name } } = props
+  const handleClick = () => {
+    props.dispatch({
+      type: "global/save",
+      payload: {
+        name: "taro-ui欢迎你的到来"
+      }
+    })
+  }
   return (
     <View className='index'>
       <Text>{name}</Text>
-      <AtButton />
+      <AtButton onClick={handleClick.bind(this)} />
     </View>
   )
 })
