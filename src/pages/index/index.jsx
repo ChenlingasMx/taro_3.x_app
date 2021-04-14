64b29b6a72baa7027b24be3f8e9981899a88e25f
WX <42308734@qq.com>
2021-04-14 17:16:07 +0800

    fix:小程序input编译问题

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -7,19 +7,12 @@ import './index.less';
 // eslint-disable-next-line
 const Index = React.forwardRef((props, ref) => {
   const {
-    global: {
-      form: { userName, passWord },
-      form,
-    },
+    global: { form },
   } = props;
 
   // 点击提交按钮 调用接口
   const handleSubmit = () => {
     window.console.log('form', form);
-    props.dispatch({
-      type: 'global/login',
-      payload: form,
-    });
   };
 
   // 输入input变化
@@ -36,20 +29,22 @@ const Index = React.forwardRef((props, ref) => {
     <View className="index">
       <AtForm>
         <AtInput
+          name="userName"
           title="账号"
-          type="userPhone"
+          type="text"
           placeholder="请输入手机号"
-          value={userName}
-          onChange={handleInputChange.bind(this, 'userName')}
+          value={form.userName}
+          onChange={handleInputChange.bind(this)}
         />
         <AtInput
+          name="passWord"
           title="密码"
           type="password"
           placeholder="请输入密码"
-          value={passWord}
-          onChange={handleInputChange.bind(this, 'passWord')}
+          value={form.passWord}
+          onChange={handleInputChange.bind(this)}
         />
-        <AtButton className="login_btn" formType="submit" onClick={handleSubmit.bind(this)}>
+        <AtButton className="login_btn" onClick={handleSubmit.bind(this)}>
           登录
         </AtButton>
       </AtForm>
